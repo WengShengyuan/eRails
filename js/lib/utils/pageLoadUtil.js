1bb7f37bfc80a93be134c1d8d5cb5a03c4ff8251
WengShengyuan <6655449987qq@gmail.com>
2015-06-05 15:48:32 +0800

    util

--- js/lib/utils/pageLoadUtil.js
+++ js/lib/utils/pageLoadUtil.js
@@ -1,29 +1,86 @@
+var util_ws;
+
+/**
+ * 当前WebView载入URL
+ * @param {Object} URL
+ */
 function currentLoadURL(URL) {
-	plus.webview.currentWebview().loadURL(URL);
+	mui.currentWebview().loadURL(URL);
 }
 
+
+/**
+ * 当前WebView隐藏
+ */
 function currentHide(){
-	plus.webview.currentWebview().hide();
+	mui.currentWebview().hide();
 }
 
+/**
+ * 当前WebView关闭
+ */
+function currentClose() {
+	mui.currentWebview().close();
+}
+
+/**
+ * 根据id载入URL
+ * @param {Object} id
+ * @param {Object} URL
+ */
 function loadURLById(id, URL){
-	var util_ws = plus.webview.getWebviewById(id).loadURL(URL);
+	util_ws = mui.getWebviewById(id).loadURL(URL);
 }
 
+/**
+ * 根据id显示
+ * @param {Object} id
+ */
 function showWebViewById (id) {
-	var util_ws = plus.webview.getWebviewById(id);
+	util_ws = mui.getWebviewById(id);
 	util_ws.show();
 }
 
+/**
+ * 根据id隐藏
+ * @param {Object} id
+ */
 function hideWebViewById (id) {
-	var util_ws = plus.webview.getWebviewById(id);
+	util_ws = mui.getWebviewById(id);
 	util_ws.hide();
 }
 
+/**
+ * 根据id关闭
+ * @param {Object} id
+ */
+function closeWebViewById(id) {
+	util_ws = mui.getWebviewById(id);
+	util_ws.close();
+}
+
+/**
+ * 显示WebView
+ * @param {Object} view
+ */
 function showWebView (view) {
 	view.show();
 }
 
+/**
+ * 隐藏WebView
+ * @param {Object} view
+ */
 function hideWebView (view) {
 	view.hide();
 }
+
+/**
+ * 关闭WebView
+ * @param {Object} view
+ */
+function closeWebView(view){
+	view.close();
+}
+
+
