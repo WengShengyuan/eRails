76b85fb46599783380dd80b7837e52d0b6ed8b65
WengShengyuan <6655449987qq@gmail.com>
2015-06-05 15:50:54 +0800

    #

--- js/lib/utils/storageUtil.js
+++ js/lib/utils/storageUtil.js
@@ -1,15 +1,31 @@
+/**
+ * 存储
+ * @param {Object} keyStr
+ * @param {Object} valueStr
+ */
 function muiSet(keyStr, valueStr){
 	plus.storage.setItem(keyStr, valueStr);
 }
 
+/**
+ * 取数
+ * @param {Object} keyStr
+ */
 function muiGet(keyStr) {
 	return plus.storage.getItem(keyStr);
 }
 
+/**
+ * 根据key删除数据
+ * @param {Object} keyStr
+ */
 function muiRemove(keyStr){
 	plus.storage.removeItem(keyStr);
 }
 
+/**
+ * 删除所有数据
+ */
 function muiRemoveAll() {
 	var length = getLength();
 	for(var i=0; i<length; i++){
@@ -18,11 +34,19 @@ function muiRemoveAll() {
 	}
 }
 
+/**
+ * 获取数据数量
+ */
 function muiGetLength() {
 	return plus.storage.getLength();
 }
 
+/**
+ * 获取某key
+ * @param {Object} id 序号
+ */
 function muiGetKey(id){
 	return plus.storage.key(id);
 }
+
 
